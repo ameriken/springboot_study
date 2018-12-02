1657791ceba1436005a8d6b7aaadb29ea339800b
ameriken <hitohahito@hotmail.co.jp>
2018-12-02 17:41:23 +0900

    ベースのEntityの作成

---
+++ src/main/java/com/example/springboot_study/entity/Course.java
@@ -0,0 +1,43 @@
+package com.example.springboot_study.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="course")
+public class Course {
+
+    private Long id;
+
+    private String title;
+
+    private Instructor instructor;
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
