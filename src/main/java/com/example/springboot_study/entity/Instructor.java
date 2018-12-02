528754af980277a9eb9db0537683b77684b14e60
ameriken <hitohahito@hotmail.co.jp>
2018-12-02 11:18:15 +0900

    単純に2つのentityを作成してみた

---
+++ src/main/java/com/example/springboot_study/entity/Instructor.java
@@ -0,0 +1,52 @@
+package com.example.springboot_study.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="instructor")
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String LastName;
+
+    @Column(name="email")
+    private String email;
+
+    public Instructor(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        LastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
